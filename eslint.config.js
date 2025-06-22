eaa1c088325a5caadec84cd3bbbda898472be020
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-06-22 15:11:25 -0400

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginRouter from "@tanstack/eslint-plugin-router";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  pluginRouter.configs["flat/recommended"],
+  pluginQuery.configs["flat/recommended"],
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  {
+    plugins: {
+      react: pluginReact.configs.flat.recommended,
+    },
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    rules: {
+      "react/react-in-jsx-scope": 0,
+      "react/jsx-uses-react": 0,
+    },
+  },
+]);
