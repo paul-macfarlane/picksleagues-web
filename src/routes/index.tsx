237e50567cf45cdf36375a2473230ba37290880e
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-06-28 14:43:53 -0400

    update routing with root domain update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,33 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { redirect } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { createFileRoute, redirect, useRouter } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: App,
+  component: RouteComponent,
   beforeLoad: async ({ context }) => {
-    if (context.session) {
-      throw redirect({ to: "/app" });
-    } else {
-      throw redirect({ to: "/welcome" });
+    if (!context.session) {
+      throw redirect({ to: "/login" });
     }
   },
 });
 
-function App() {
-  return <></>;
+function RouteComponent() {
+  const { navigate } = useRouter();
+  const { data: session } = authClient.useSession();
+
+  async function signOut() {
+    try {
+      await authClient.signOut();
+      navigate({ to: "/login", reloadDocument: true });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center p-4 bg-background gap-4">
+      <p>Hello {session?.user?.name}!</p>
+      <Button onClick={signOut}>Sign out</Button>
+    </div>
+  );
 }
