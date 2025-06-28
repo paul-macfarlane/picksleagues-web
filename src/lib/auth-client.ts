72f93f2fb9719f92d476c291f5302a3bd49fa5a5
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-06-28 11:16:35 -0400

    credentials: include

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,9 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
+  fetchOptions: {
+    credentials: "include",
+  },
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: import.meta.env.VITE_BETTER_AUTH_URL!,
 });
