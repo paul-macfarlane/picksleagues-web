870f21ed704137577a72e280b8b8387d8fbfcf52
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-07-06 17:50:10 -0400

    close sheet menu on click

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   NavigationMenu,
   NavigationMenuList,
@@ -65,13 +65,20 @@ function ProfileMenuError() {
 }
 
 function Navbar({ rightContent }: { rightContent: React.ReactNode }) {
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
+
   return (
     <nav className="w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 sticky top-0 z-40">
       <div className="container mx-auto px-4 flex h-16 items-center justify-between gap-2">
         <div className="md:hidden flex items-center">
-          <Sheet>
+          <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
             <SheetTrigger asChild>
-              <Button variant="ghost" size="icon" className="mr-2">
+              <Button
+                variant="ghost"
+                size="icon"
+                className="mr-2"
+                onClick={() => setIsSheetOpen(!isSheetOpen)}
+              >
                 <MenuIcon className="w-6 h-6" />
                 <span className="sr-only">Open menu</span>
               </Button>
@@ -85,6 +92,7 @@ function Navbar({ rightContent }: { rightContent: React.ReactNode }) {
                 <Link
                   to="/"
                   className="flex items-center gap-2 rounded hover:bg-accent"
+                  onClick={() => setIsSheetOpen(false)}
                 >
                   <Home className="w-5 h-5" /> Home
                 </Link>
@@ -96,6 +104,7 @@ function Navbar({ rightContent }: { rightContent: React.ReactNode }) {
                     <Link
                       to="/football/pickem"
                       className="flex items-center gap-2 py-2 px-2 rounded"
+                      onClick={() => setIsSheetOpen(false)}
                     >
                       <CheckSquare className="w-4 h-4" /> Pick'em
                     </Link>
