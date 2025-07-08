c78d6cb27ca7c652526fb8fad3013a3308f79752
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-07-07 21:03:29 -0400

    remove old files

--- src/routes/football/pickem/$leagueId.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/football/pickem/$leagueId")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/football/pickem/$leagueId"!</div>;
-}
