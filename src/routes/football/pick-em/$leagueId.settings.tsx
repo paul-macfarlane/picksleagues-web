402bfa91ab61aa4ed6fe77714841123037cdfcb0
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-07-17 20:41:46 -0400

    final data fetching update

--- src/routes/football/pick-em/$leagueId.settings.tsx
+++ src/routes/football/pick-em/$leagueId.settings.tsx
@@ -1,5 +1,9 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { LeagueSettingsForm } from "@/features/leagues/components/league-settings-form";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { GetLeagueMembersQueryOptions } from "@/features/leagueMembers/leagueMembers.api";
+import { LEAGUE_MEMBER_ROLES } from "@/features/leagueMembers/leagueMembers.types";
+import { Route as LeagueLayoutRoute } from "./$leagueId";
 
 export const Route = createFileRoute("/football/pick-em/$leagueId/settings")({
   component: SettingsComponent,
@@ -8,15 +12,30 @@ export const Route = createFileRoute("/football/pick-em/$leagueId/settings")({
       throw redirect({ to: "/login" });
     }
   },
+  loader: async ({ context: { queryClient }, params: { leagueId } }) => {
+    await queryClient.ensureQueryData(GetLeagueMembersQueryOptions(leagueId));
+  },
 });
 
 function SettingsComponent() {
-  // TODO: Refactor to get isCommissioner and seasonState from API
-  const isCommissioner = false;
-  const isOffSeason = false;
+  const { leagueId } = Route.useParams();
+  const { session } = Route.useRouteContext();
+  const league = LeagueLayoutRoute.useLoaderData();
+  const { data: members } = useSuspenseQuery(
+    GetLeagueMembersQueryOptions(leagueId),
+  );
+
+  // todo could add an endpoint to get a user's role in a league, as opposed to fetching all members
+  const currentUserMemberInfo = members.find(
+    (member) => member.userId === session?.userId,
+  );
+  const isCommissioner =
+    currentUserMemberInfo?.role === LEAGUE_MEMBER_ROLES.COMMISSIONER;
+  const isOffSeason = true; // TODO: get from league season state
 
   return (
     <LeagueSettingsForm
+      league={league}
       isCommissioner={isCommissioner}
       isOffSeason={isOffSeason}
     />
