aafe45ce86a2b01c862604ff02d4bff7ed67d232
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-07-17 23:26:14 -0400

    add note about issue on members tab

--- src/routes/football/pick-em/$leagueId.members.tsx
+++ src/routes/football/pick-em/$leagueId.members.tsx
@@ -1,13 +1,17 @@
 import { createFileRoute, redirect, useParams } from "@tanstack/react-router";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { LEAGUE_MEMBER_ROLES } from "@/features/leagueMembers/leagueMembers.types";
+import {
+  LEAGUE_MEMBER_INCLUDES,
+  LEAGUE_MEMBER_ROLES,
+} from "@/features/leagueMembers/leagueMembers.types";
 import { GetLeagueMembersQueryOptions } from "@/features/leagueMembers/leagueMembers.api";
 import { MembersList } from "@/features/leagueMembers/components/members-list";
 import { InviteManagement } from "@/features/leagueInvites/components/invite-management";
 import { GetLeagueInvitesQueryOptions } from "@/features/leagueInvites/leagueInvites.api";
 import { MembersPageSkeleton } from "@/features/leagueMembers/components/members-page-states";
 import { RouteErrorBoundary } from "@/components/route-error-boundary";
+import { LEAGUE_INVITE_INCLUDES } from "@/features/leagueInvites/leagueInvites.types";
 
 export const Route = createFileRoute("/football/pick-em/$leagueId/members")({
   component: MembersComponent,
@@ -18,8 +22,19 @@ export const Route = createFileRoute("/football/pick-em/$leagueId/members")({
   },
   loader: async ({ context: { queryClient }, params: { leagueId } }) => {
     await Promise.all([
-      queryClient.ensureQueryData(GetLeagueMembersQueryOptions(leagueId)),
-      queryClient.ensureQueryData(GetLeagueInvitesQueryOptions({ leagueId })),
+      queryClient.ensureQueryData(
+        GetLeagueMembersQueryOptions(leagueId, [
+          LEAGUE_MEMBER_INCLUDES.PROFILE,
+        ]),
+      ),
+
+      // TODO - there is an issue where only league commissioners can see invites, so this will return a 403. Need to look into a way to see if we can conditionally prefetch
+      queryClient.ensureQueryData(
+        GetLeagueInvitesQueryOptions({
+          leagueId,
+          includes: [LEAGUE_INVITE_INCLUDES.INVITEE],
+        }),
+      ),
     ]);
   },
   pendingComponent: MembersPageSkeleton,
@@ -33,10 +48,13 @@ function MembersComponent() {
   });
   const { session } = Route.useRouteContext();
   const { data: members } = useSuspenseQuery(
-    GetLeagueMembersQueryOptions(leagueId),
+    GetLeagueMembersQueryOptions(leagueId, [LEAGUE_MEMBER_INCLUDES.PROFILE]),
   );
   const { data: invites } = useSuspenseQuery(
-    GetLeagueInvitesQueryOptions({ leagueId }),
+    GetLeagueInvitesQueryOptions({
+      leagueId,
+      includes: [LEAGUE_INVITE_INCLUDES.INVITEE],
+    }),
   );
 
   const currentUserMemberInfo = members.find(
